98e2849d03f485a2389cdaace10a086d28fb6d52
dmchub <37676729+dmchub@users.noreply.github.com>
2021-08-06 01:42:52 +0300

    Added services and repository for the project

---
+++ src/main/java/com/example/ecommerce/service/ProductServiceImpl.java
@@ -0,0 +1,20 @@
+package com.example.ecommerce.service;
+
+import com.example.ecommerce.model.Product;
+
+public class ProductServiceImpl implements ProductService {
+    @Override
+    public Iterable<Product> getAllProducts() {
+        return null;
+    }
+
+    @Override
+    public Product getProduct(long id) {
+        return null;
+    }
+
+    @Override
+    public Product save(Product product) {
+        return null;
+    }
+}
